3b4a5e11e5f9b42ca360d8d68d2a40ab071d200f
kitopang <kitop40@gmail.com>
2022-06-24 21:32:57 -0700

    test

---
+++ app.js
@@ -0,0 +1,19 @@
+let dataFile;
+
+// const options = {
+//     method: 'GET',
+//     url: 'https://priaid-symptom-checker-v1.p.rapidapi.com/symptoms',
+//     params: { language: 'en-gb', format: 'json' },
+//     headers: {
+//         'X-RapidAPI-Key': '3be63b2714msh9d243fd4ef7dd1ap1632ebjsn81dc7e84b888',
+//         'X-RapidAPI-Host': 'priaid-symptom-checker-v1.p.rapidapi.com'
+//     }
+// };
+
+// axios.request(options).then(function (response) {
+//     dataFile = response.data;
+//     console.log(response.data);
+// }).catch(function (error) {
+//     console.error(error);
+// });
+
