fdabd01994b2f38eefe049a9c8e68504aeec18e8
kitopang <kitop40@gmail.com>
2022-07-01 20:36:11 -0700

    don't use

--- app.js
+++ app.js
@@ -1,19 +1,31 @@
-let dataFile;
+let symptomsLog = "Nothing";
+let symptomsArray = []
 
-// const options = {
-//     method: 'GET',
-//     url: 'https://priaid-symptom-checker-v1.p.rapidapi.com/symptoms',
-//     params: { language: 'en-gb', format: 'json' },
-//     headers: {
-//         'X-RapidAPI-Key': '3be63b2714msh9d243fd4ef7dd1ap1632ebjsn81dc7e84b888',
-//         'X-RapidAPI-Host': 'priaid-symptom-checker-v1.p.rapidapi.com'
-//     }
-// };
+const options = {
+    method: 'GET',
+    url: 'https://priaid-symptom-checker-v1.p.rapidapi.com/symptoms',
+    params: { language: 'en-gb', format: 'json' },
+    headers: {
+        'X-RapidAPI-Key': '3be63b2714msh9d243fd4ef7dd1ap1632ebjsn81dc7e84b888',
+        'X-RapidAPI-Host': 'priaid-symptom-checker-v1.p.rapidapi.com'
+    }
+};
 
-// axios.request(options).then(function (response) {
-//     dataFile = response.data;
-//     console.log(response.data);
-// }).catch(function (error) {
-//     console.error(error);
-// });
+axios.request(options).then(function (response) {
+    console.log(response.data);
+}).catch(function (error) {
+    console.error(error);
+});
+
+
+
+
+// for (let symptom of symptomsLog) {
+//     symptomsArray.push(symptom);
+// }
+// console.log(symptomsArray);
+
+// for (let symptom of symptomsArray) {
+//     console.log(symptom);
+// }
 
